09c734e4f2fdcfbc62fa66f36ae4e74262d9a204
YuRuolalala <2386402407@qq.com>
2022-10-05 18:03:46 +0800

    SpringMVC 初步整合前端

---
+++ SpringMVC_Demo09_page/src/main/java/com/controller/ProjectExceptionAdvice.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import com.exception.BusinessException;
+import com.exception.SystemException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ProjectExceptionAdvice {
+
+    @ExceptionHandler(BusinessException.class)
+    public Result doBusinessException(BusinessException e){
+        return new Result(e.getCode(),null,e.getMessage());
+    }
+
+    @ExceptionHandler(SystemException.class)
+    public Result doSystemException(SystemException e){
+        return new Result(e.getCode(),null,e.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public Result doException(Exception e){
+        System.out.println("异常往哪跑？！");
+        return new Result(666,null,"系统繁忙，请联系管理员");
+    }
+}
